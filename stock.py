dce238581e0677a4422405512c004e562aaf94e0
chella kasinath <114824398+kasinath88@users.noreply.github.com>
2024-05-23 14:47:30 +0530

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import plotly.express as px
+import yfinance as yf
+
+st.title("Prabhas Movie Budgets")
+st.write("This is all prabhas movies all budgets.")
+file = pd.read_csv('hello.csv')
+movie = file['movie_name']
+budget = file['budget']
+fig = px.bar(x=movie,y=budget,title="prabhas movies budgets",labels={'x':'movie name','y':'Budget'})
+st.plotly_chart(fig)
+
+fig1 = px.line(x=movie,y=budget,title="prabhas movies budgets",labels={'x':'movie name','y':'Budget'})
+st.plotly_chart(fig1)
+
+fig2 = px.box(x=movie,y=budget,title="prabhas movies budgets",labels={'x':'movie name','y':'Budget'})
+st.plotly_chart(fig2)
+
+fig3 = px.area(x=movie,y=budget,title="prabhas movies budgets",labels={'x':'movie name','y':'Budget'})
+st.plotly_chart(fig3)
+
+fig4 = px.funnel(x=movie,y=budget,title="prabhas movies budgets",labels={'x':'movie name','y':'Budget'})
+st.plotly_chart(fig4)
